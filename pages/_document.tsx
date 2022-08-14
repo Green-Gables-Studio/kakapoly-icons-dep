b6e914f8ebb721170e4f590d3ac7d9b51d5210b9
Alex Kwon <khwalex@gmail.com>
2022-08-14 18:20:23 +0900

    Add basic settings
    - Added fonts and font family settings
    - Added i18n settings for internationalization with a minimum translation.

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* eslint-disable-next-line @next/next/no-css-tags */}
+          <link href="/fonts/inter.css" rel="stylesheet" />
+          {/* eslint-disable-next-line @next/next/no-css-tags */}
+          <link href="/fonts/pretendard.css" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
